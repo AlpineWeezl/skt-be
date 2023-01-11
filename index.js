9de3c72ed40541f427e895673b959ea9a8580554
Matthias Wieser <matthias.wieser.83@live.de>
2023-01-11 14:36:03 +0100

    fix - solve merge conflicts

--- index.js
+++ index.js
@@ -2,15 +2,8 @@ import "dotenv/config";
 import express from "express";
 import "./src/db/client.js";
 import cors from "cors";
-<<<<<<< HEAD
-import swaggerUi from "swagger-ui-express";
-import { usersRouter } from "./src/routers/usersRouter.js";
-import { eventsRouter } from "./src/routers/eventsRouter.js";
-import { specs } from "./src/doc/swagger.js";
-=======
 import { userRouter } from "./src/routers/userRouter.js";
 import { tournamentRouter } from "./src/routers/tournamentRouter.js";
->>>>>>> beb9492 (fix - doublette check)
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -23,24 +16,13 @@ const corsOptions = {
 	origin: process.env.REACT_APP, // Zugriff nur von dieser Domain erlauben
 	exposedHeaders: "Authorization", //dem Frontend Zugriff auf die Header-Property "Authorization" geben
 };
-<<<<<<< HEAD
-// app.use(express());
-=======
->>>>>>> beb9492 (fix - doublette check)
 app.use(cors(corsOptions));
 
 // ############################################################################################
 // ----------------------------- Router -------------------------------------------------------
 // ############################################################################################
-<<<<<<< HEAD
-app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(specs));
-// console.log(specs);
-app.use("/api/users", usersRouter);
-app.use("/api/events", eventsRouter);
-=======
 // console.log(specs);
 app.use("/api/users", userRouter);
 app.use("/api/events", tournamentRouter);
->>>>>>> beb9492 (fix - doublette check)
 
 app.listen(port, () => console.log(`The server is running on port ${port}`));
--- src/middlewares/userChecks.js
+++ src/middlewares/userChecks.js
@@ -3,33 +3,6 @@
 
 import { User } from "../models/User.js";
 
-<<<<<<< HEAD
-// const schema = object()
-//     .keys({
-//         email:
-//             string()
-//                 .email()
-//                 .required(),
-//         username:
-//             string()
-//                 .min(4)
-//                 .max(40)
-//                 .required(),
-//         password:
-//             pass()
-//                 .min(8)
-//                 .max(127)
-//                 .required(),
-//     })
-// const data = {
-//     name: 'Srajan',
-//     age: 10
-// };
-// export const result = schema.validate(data);
-// console.log(result);
-
-// ------------------------------------ double email check ------------------------------------------------
-=======
 // ------------------------------------ double email check ------------------------------------------------
 /**
  * This function checks, if the desired username is already taken
@@ -37,23 +10,15 @@ import { User } from "../models/User.js";
  * @param {*} res Response
  * @param {*} next Next function in router chain
  */
->>>>>>> beb9492 (fix - doublette check)
 export const doubleEmailUsernameCheck = (req, res, next) => {
     const { email, username } = req.body.user;
     const existingEmail = User.find({ email: email });
     console.log(existingEmail);
     const existingUsername = User.findOne({ username: username });
-<<<<<<< HEAD
-    if (existingEmail) {
-        console.log('email exists');
-        res.status(400).json({ message: "user cannot be created!" });
-    } else if (existingUsername) {
-=======
     if (existingEmail && existingEmail.email !== email) {
         console.log('email exists');
         res.status(400).json({ message: "user cannot be created!" });
     } else if (existingUsername && existingEmail.email !== email) {
->>>>>>> beb9492 (fix - doublette check)
         console.log('username exists');
         res.status(400).json({ message: "user cannot be created!" });
     } else {
